0c2a4f4cadb49631b587cda803cf8bfb4d2c080a
Udayraj <for.udayraj@gmail.com>
2023-08-12 12:45:31 +0530

    added variable.js

---
+++ variable.js
@@ -0,0 +1,10 @@
+const accountId=4562
+let nama = "udayraj"
+var age = 21
+
+console.log(accountId)
+console.log(nama)
+console.log(age)
+//prefer not to user var keyword to assign a variable
+//user let because avoid bloack scope and functional scope conflict
+// we cannot reassign a variable of const  S
