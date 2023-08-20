b006a61b986624d65b91465f1006153f90a77031
Udayraj <for.udayraj@gmail.com>
2023-08-20 09:46:17 +0530

    loops

--- controlFlow/forloop.js
+++ controlFlow/forloop.js
@@ -1,20 +1,20 @@
-// for(index = 1; index < 10; index++){
-//     const ele= index;
-//     if (ele == 5){
-//         console.log("5 is best Number");
-//     }
-//     // console.log(ele);
-// }
+for(index = 1; index < 10; index++){
+    const ele= index;
+    if (ele == 5){
+        console.log("5 is best Number");
+    }
+    // console.log(ele);
+}
 
-// for(i = 1; i<=10;i++){
-//     console.log(`Outer loop:${i}`);
-//     for(j=1;j<=10;j++){
-//         //console.log(`Inner Loop:${j} and inner loop ${i}`);
-//         console.log(i+"*"+j+
-//         "="+i*j);
+for(i = 1; i<=10;i++){
+    console.log(`Outer loop:${i}`);
+    for(j=1;j<=10;j++){
+        //console.log(`Inner Loop:${j} and inner loop ${i}`);
+        console.log(i+"*"+j+
+        "="+i*j);
 
-//     }
-// }
+    }
+}
 
 let myArray = ["ironman", "batman", "flash", "wonder women"]
 console.log(myArray.length);
@@ -42,4 +42,13 @@ for (i =1; i<=20; i++){
         continue
     }
     console.log(`value of ${i}`);
+}
+
+const  arr =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (i = 1; i<=arr.length;i++){
+    if(arr[i]%2 == 0){
+        let a = arr[i]
+        console.log(a);
+    }
 }
