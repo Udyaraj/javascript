12078c3f8b3296366bdba8dfeba5c35a16c59597
Udayraj <for.udayraj@gmail.com>
2024-01-05 00:35:16 +0530

    new commit

---
+++ async/async.js
@@ -0,0 +1,5 @@
+//javascript = synchronous, single threaded
+//execution context = execute on line of code at a one time
+//blocking code means block the flow of program ex read the file sync
+//non-blocking read the file async
+//file access is avaliable only in javascript
